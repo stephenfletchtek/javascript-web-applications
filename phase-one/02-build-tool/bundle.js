8dda6adadcc539a7e9e92561cde4808aada17fe3
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-30 15:40:12 +0100

    02 done

--- phase-one/02-build-tool/bundle.js
+++ phase-one/02-build-tool/bundle.js
@@ -1,5 +1,33 @@
 (() => {
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // add.js
+  var require_add = __commonJS({
+    "add.js"(exports, module) {
+      add = (a, b) => {
+        return a + b;
+      };
+      module.exports = add;
+    }
+  });
+
+  // multiply.js
+  var require_multiply = __commonJS({
+    "multiply.js"(exports, module) {
+      multiply = (a, b) => {
+        return a * b;
+      };
+      module.exports = multiply;
+    }
+  });
+
   // index.js
+  var add2 = require_add();
+  var multiply2 = require_multiply();
   console.log("Hello from the developer console!");
   console.log(new Date());
+  console.log(multiply2(add2(2, 2), 4));
 })();
