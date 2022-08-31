d49d1c68cd06be649d5f03ebbe08b6739c7a3372
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-31 17:32:11 +0100

    hide messages

--- phase-one/08-user-interaction/messageView.js
+++ phase-one/08-user-interaction/messageView.js
@@ -1,10 +1,15 @@
 class MessageView {
   constructor() {
-    this.buttonEl = document.querySelector('#show-message-button');
-    this.mainContainerEl = document.querySelector('#main-container')
+    this.showButtonEl = document.querySelector('#show-message-button');
+    this.hideButtonEl = document.querySelector('#hide-message-button');
+    this.mainContainerEl = document.querySelector('#main-container');
 
-    this.buttonEl.addEventListener('click', () => {
+    this.showButtonEl.addEventListener('click', () => {
       this.displayMessage();
+    });
+
+    this.hideButtonEl.addEventListener('click', () => {
+      this.hideMessage();
     });
   }
 
@@ -14,6 +19,16 @@ class MessageView {
     divEl.id = 'message';
     this.mainContainerEl.append(divEl);
     console.log('Thanks for clicking me!');
+  }
+
+  hideMessage() {
+    const messages = document.querySelectorAll('#message')
+    messages.forEach((element) => {
+      element.remove()
+    })
+
+    // const message = document.querySelector('#message')
+    // message.remove()
   }
 }
 
--- phase-one/08-user-interaction/messageView.test.js
+++ phase-one/08-user-interaction/messageView.test.js
@@ -16,4 +16,17 @@ describe('MessageView', () => {
 
     expect(document.querySelector('#message')).not.toBeNull();
   });
+
+  it('click the show button followed by the hide button', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const view = new MessageView();
+
+    const showButtonEl = document.querySelector('#show-message-button');
+    const hideButtonEl = document.querySelector('#hide-message-button');
+    showButtonEl.click();
+    hideButtonEl.click();
+
+    expect(document.querySelector('#message')).toBeNull();
+  });
 });
