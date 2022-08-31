f64a2eaa7e795196d2dfb2cf110736bde5e1cb06
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-31 13:58:40 +0100

    add beforeEach method to view.test.js

--- phase-one/05-interacting-with-page/view.test.js
+++ phase-one/05-interacting-with-page/view.test.js
@@ -6,20 +6,18 @@ const fs = require('fs');
 const View = require('./view');
 
 describe('Page view', () => {
+  beforeEach(() => {
+    document.body.innerHTML = fs.readFileSync('./index.html')
+  });
+
   it('displays 2 paragraphs', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
-
     const view = new View();
-
     expect(document.querySelectorAll('p').length).toBe(2);
   });
 
   it('displays 3 paragraphs', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
-
     const view = new View();
     view.addParagraph('Extra Smurf paragraph!')
-
     expect(document.querySelectorAll('p').length).toBe(3);
   });
 
