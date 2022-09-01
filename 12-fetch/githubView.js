3020a5a458c0c7c75b357266fbcc026ccb4135fb
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-01 14:16:34 +0100

    display on wab page.

--- 12-fetch/githubView.js
+++ 12-fetch/githubView.js
@@ -6,17 +6,17 @@ class GithubView {
     const submitButtonEl = document.querySelector('#submit-button');
     const repoInputEl = document.querySelector('#repo-name-input');
 
+
     submitButtonEl.addEventListener('click', () => {
       const repoName = repoInputEl.value;
 
-      this.api.getRepoInfo(repoName, repoData => {
-        console.log(repoData);
-      });
+      this.api.getRepoInfo(repoName, (repoData) => this.display(repoData));
     });
   }
 
-  display() {
-
+  display(repoData) {
+    document.querySelector('#repo-name').textContent = repoData.full_name
+    document.querySelector('#repo-description').textContent = repoData.description
   }
 }
 
